7576bfdcefc5297ec30a2c93d558215e8e186351
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-12-01 14:06:25 -0800

    Created logger and validate user id, updated route handlers

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,19 @@
+const User = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('\nLOGGER: ', { method: req.method, url: req.url, time: Date.now() });
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  User.getById(req.params.id)
+    .then( response => {
+      req.user = response;
+      next();
+    })
+    .catch( err => {  
+      res.status(404).json({ message: "user not found" })
+    })
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +25,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const usersRouter = require('./users/users-router');
-
 const server = express();
+const { logger } = require('./middleware/middleware');
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+server.use(logger);
 server.use('/api', usersRouter);
 
 server.get('/', (req, res) => {
@@ -17,6 +18,5 @@ server.get('/', (req, res) => {
 //   // catch all 404 errors middleware
 //   res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
 // });
-
 
 module.exports = server;
